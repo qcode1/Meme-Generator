6297aeefe9a37355ccd67c99513a40c7ffb7ac8b
qcode1 <nii.quartey19@gmail.com>
2022-06-29 10:56:01 +0000

    Handling API call via useEffect

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { MemesData } from "../memesData"
 
 export default function Form(props) {
 
@@ -10,12 +9,17 @@ export default function Form(props) {
     })
 
     // eslint-disable-next-line no-unused-vars
-    const [allMemeImages, setAllMemeImages] = React.useState(MemesData)
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    }, [])
 
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
